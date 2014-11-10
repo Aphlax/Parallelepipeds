938c6dd9f276ed5ce9344000f39948bef54f61db
walteser <walteser@something.ch>
2014-11-10 13:14:13 +0100

    openMPCC added

--- connected-components/src/openMPCC.cpp
+++ connected-components/src/openMPCC.cpp
@@ -4,20 +4,36 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/graph/connected_components.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace boost;
+
 
 typedef adjacency_list<vecS, vecS, undirectedS> Graph;
 typedef graph_traits<Graph>::vertex_descriptor Vertex;
 
 int run(const int numberOfVertices, const std::vector<std::pair<int,int> > &edges, std::vector<int> &outVertexToComponent) {
-	Graph g(numberOfVertices);
-	for (unsigned int i = 0; i < edges.size(); ++i) {
-		add_edge(edges[i].first, edges[i].second,g);
+	int numThreads = omp_get_num_threads();
+	int componentCount=0;
+	cout << "run MPCC";
+
+	#pragma omp parallel reduction(+:componentCount)
+	{
+		int tid = omp_get_thread_num();
+		int rangeSize = edges.size()/numThreads;
+		int rangeStart = tid*rangeSize;
+		int rangeEnd = rangeStart + rangeSize;
+
+		Graph g(numberOfVertices);
+		for (unsigned int i = rangeStart; i < rangeSize; ++i) {
+			add_edge(edges[i].first, edges[i].second,g);
+		}
+		componentCount = connected_components(g, &outVertexToComponent[0]);
 	}
 
-	int componentCount = connected_components(g, &outVertexToComponent[0]);
+
 	return componentCount;
 }
+
 
