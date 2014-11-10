a82835a620fe4b34cdfaebf1180038d2fb70efe7
Fabian Meier <meiefabi@student.ethz.ch>
2014-11-10 13:27:07 +0100

    Added union find to eclipse project

---
+++ connected-components/src/SerialUnionFind.cpp
@@ -0,0 +1,59 @@
+
+#include "SerialUnionFind.h"
+
+# include <iostream>
+# include <vector>
+# include <utility>
+# include <algorithm>
+# include <queue>
+
+using namespace std;
+
+int SerialUnionFind::run(const int numberOfVertices, const std::vector<std::pair<int,int> > &edges, std::vector<int> &outVertexToComponent) {
+	int n = numberOfVertices;
+	int m = edges.size();
+	vector<int> comp(n);
+	for (int i = 0; i < n; i++) {
+		comp[i] = i;
+	}
+
+	for (int i = 0; i < m; i++) {
+		int u = edges[i].first;
+		int v = edges[i].second;
+		u = find(comp, u)
+		v = find(comp, v)
+
+		if (u != v) {
+			unio(comp, u, v)
+			n--;
+		}
+	}
+
+	cout << n << endl;
+
+	for (int i = 0; i < n; i++) {
+		outVertexToComponent[i] = find(comp, i);
+	}
+
+	return 0;
+}
+
+// Returns the set of that element
+inline int find(vector<int> comp, int find) {
+	int last = -1;
+	while (comp[find] != find) {
+		if (last != -1)
+			comp[last] = comp[find]
+		last = find
+		find = comp[find]
+	}
+}
+
+// Unions two sets
+inline void unio(vector<int> comp, int u, int v) {
+	if (u > v)
+		comp[u] = v
+	else
+		comp[v] = u
+}
+
---
+++ connected-components/src/SerialUnionFind.h
@@ -0,0 +1,21 @@
+
+#ifndef __SERIAL_UNION_FIND_H_INCLUDED__
+#define __SERIAL_UNION_FIND_H_INCLUDED__
+
+# include <iostream>
+# include <vector>
+# include <utility>
+# include <algorithm>
+# include <queue>
+
+class SerialUnionFind {
+
+public:
+	/**
+	 * returns total number of components and fills out_vertex_to_component filled with each vertex's component.
+	 * It expects that outVertexToComponent is a vector of size numberOfVertices, filled with -1
+	 */
+	int run(const int numberOfVertices, const std::vector<std::pair<int,int> > &edges, std::vector<int> &outVertexToComponent);
+};
+
+#endif
