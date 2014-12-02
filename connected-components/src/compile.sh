cd7d33a8d77589f62d6933985b5215fc342262b0
Fabian Meier <meiefabi@student.ethz.ch>
2014-12-02 18:31:48 +0100

    Complie command

---
+++ connected-components/src/compile.sh
@@ -0,0 +1,1 @@
+icc -mmic -fopenmp main.cpp Boost.cpp RandomGraph.cpp Bfs.cpp openMPCC.cpp pBoost.cpp SerialUnionFind.cpp RandomizedContract.cpp PRandomizedContract.cpp SpanningTreeCC.cpp pBfsAtomic.cpp -std=c++11 -o pcc.mic
