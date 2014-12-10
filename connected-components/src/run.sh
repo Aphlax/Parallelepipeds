fd3a7fa880d391f7df63e5a4e91538b94fb17c80
Fabian Meier <meiefabi@student.ethz.ch>
2014-12-10 16:15:43 +0100

    Added job file to repo

--- connected-components/src/run.sh
+++ connected-components/src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #SBATCH --gres=mic:1 -n 1 -N 1
 export SINK_LD_LIBRARY_PATH=/opt/intel/composer_xe_2015/lib/mic/
-srun --gres=mic:1 micnativeloadex ./a.mic -g graphs/graph04.txt bfs
+srun --gres=mic:1 micnativeloadex ./pcc.mic
