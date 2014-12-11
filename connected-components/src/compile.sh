2d4339296873175bdd23abad59d0729a97923efa
Fabian Meier <meiefabi@student.ethz.ch>
2014-12-11 23:04:55 +0100

    updated compile commands

--- connected-components/src/compile.sh
+++ connected-components/src/compile.sh
@@ -1,1 +1,12 @@
-icc -mmic -fopenmp main.cpp Boost.cpp RandomGraph.cpp Bfs.cpp openMPCC.cpp pBoost.cpp SerialUnionFind.cpp RandomizedContract.cpp PRandomizedContract.cpp SpanningTreeCC.cpp pBfsAtomic.cpp -std=c++11 -o pcc.mic
+non-mpi:
+icc -mmic -fopenmp main.cpp Boost.cpp RandomGraph.cpp Bfs.cpp OpenMPCC.cpp pBoost.cpp SerialUnionFind.cpp RandomizedContract.cpp PRandomizedContract.cpp SpanningTreeCC.cpp pBfsAtomic.cpp -std=c++11 -o pcc.mic -I"../boost/boost_1_57_0" 
+
+
+mpi (our own which does not work):
+/home/dphpc19/mpi/bin/mpic++ -mmic -fopenmp main.cpp Boost.cpp RandomGraph.cpp Bfs.cpp OpenMPCC.cpp pBoost.cpp SerialUnionFind.cpp RandomizedContract.cpp PRandomizedContract.cpp SpanningTreeCC.cpp pBfsAtomic.cpp -std=c++11 -o pcc.mic -I"../boost/boost_1_57_0" 
+
+mpi (sent by timo, not tested!):
+/opt/intel/impi/5.0.1.035/bin64/mpiicc -mmic -fopenmp main.cpp Boost.cpp RandomGraph.cpp Bfs.cpp OpenMPCC.cpp pBoost.cpp SerialUnionFind.cpp RandomizedContract.cpp PRandomizedContract.cpp SpanningTreeCC.cpp pBfsAtomic.cpp -std=c++11 -o pcc.mic -I"../boost/boost_1_57_0" 
+
+
+sbatch ./run.sh
