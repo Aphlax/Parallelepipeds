61c3313b4ada9d80f7601d7796e1e77c5177104a
Gustavo Segovia <gustavo.segovia@gmail.com>
2014-12-13 17:36:42 +0100

    commenting out PBoost

--- connected-components/src/PBoost.cpp
+++ connected-components/src/PBoost.cpp
@@ -1,118 +1,121 @@
 #include "PBoost.h"
 
-#ifdef __linux__
-// Copyright (C) 2004-2008 The Trustees of Indiana University.
 
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-//  Authors: Douglas Gregor
-//           Andrew Lumsdaine
-
-// Example usage of breadth_first_search algorithm
-
-// Enable PBGL interfaces to BGL algorithms
-#include <boost/graph/use_mpi.hpp>
-
-// Communicate via MPI
-#include <boost/graph/distributed/mpi_process_group.hpp>
-
-// Breadth-first search algorithm
-#include <boost/graph/breadth_first_search.hpp>
-
-// Distributed adjacency list
-#include <boost/graph/distributed/adjacency_list.hpp>
-
-// METIS Input
-#include <boost/graph/metis.hpp>
-
-// Graphviz Output
-#include <boost/graph/distributed/graphviz.hpp>
-
-// For choose_min_reducer
-#include <boost/graph/distributed/distributed_graph_utility.hpp>
-#include <boost/property_map/parallel/distributed_property_map.hpp>
-
-// Standard Library includes
-#include <fstream>
-#include <string>
-
-#include <boost/graph/connected_components.hpp>
-#include <ctime>
-#include <chrono>
-#include <algorithm>
-
-#ifdef BOOST_NO_EXCEPTIONS
-void
-boost::throw_exception(std::exception const& ex)
-{
-    std::cout << ex.what() << std::endl;
-    abort();
-}
-#endif
-
-using namespace boost;
-using boost::graph::distributed::mpi_process_group;
-
-/* An undirected graph with distance values stored on the vertices. */
-struct component_t {
-        typedef vertex_property_tag kind;
-};
-typedef property<component_t, double> VertexComponent;
-typedef adjacency_list<vecS, distributedS<mpi_process_group, vecS>, undirectedS, VertexComponent> Graph;
-
-typedef graph_traits<Graph>::vertex_descriptor Vertex;
-
-using namespace std;
-
+//#ifdef __linux__
+//// Copyright (C) 2004-2008 The Trustees of Indiana University.
+//
+//// Use, modification and distribution is subject to the Boost Software
+//// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//// http://www.boost.org/LICENSE_1_0.txt)
+//
+////  Authors: Douglas Gregor
+////           Andrew Lumsdaine
+//
+//// Example usage of breadth_first_search algorithm
+//
+//// Enable PBGL interfaces to BGL algorithms
+//#include <boost/graph/use_mpi.hpp>
+//
+//// Communicate via MPI
+//#include <boost/graph/distributed/mpi_process_group.hpp>
+//
+//// Breadth-first search algorithm
+//#include <boost/graph/breadth_first_search.hpp>
+//
+//// Distributed adjacency list
+//#include <boost/graph/distributed/adjacency_list.hpp>
+//
+//// METIS Input
+//#include <boost/graph/metis.hpp>
+//
+//// Graphviz Output
+//#include <boost/graph/distributed/graphviz.hpp>
+//
+//// For choose_min_reducer
+//#include <boost/graph/distributed/distributed_graph_utility.hpp>
+//#include <boost/property_map/parallel/distributed_property_map.hpp>
+//
+//// Standard Library includes
+//#include <fstream>
+//#include <string>
+//
+//#include <boost/graph/connected_components.hpp>
+//#include <ctime>
+//#include <chrono>
+//#include <algorithm>
+//
+//#ifdef BOOST_NO_EXCEPTIONS
+//void
+//boost::throw_exception(std::exception const& ex)
+//{
+//    std::cout << ex.what() << std::endl;
+//    abort();
+//}
+//#endif
+//
+//using namespace boost;
+//using boost::graph::distributed::mpi_process_group;
+//
+///* An undirected graph with distance values stored on the vertices. */
+//struct component_t {
+//        typedef vertex_property_tag kind;
+//};
+//typedef property<component_t, double> VertexComponent;
+//typedef adjacency_list<vecS, distributedS<mpi_process_group, vecS>, undirectedS, VertexComponent> Graph;
+//
+//typedef graph_traits<Graph>::vertex_descriptor Vertex;
+//
+//
+//using namespace std;
+//
+//
+//int PBoost::run(const int numberOfVertices, const std::vector<std::pair<int,int> > &edges, std::vector<int> &outVertexToComponent, StopWatch &stopWatch) {
+//
+//	boost::mpi::environment env();
+//
+//
+//	stopWatch.start(stopWatch.inputProcessing);
+//
+//	Graph g(numberOfVertices);
+//	if (process_id(g.process_group()) == 0) {
+//		int mpiProcessRank;
+//		MPI_Comm_rank (MPI_COMM_WORLD, &mpiProcessRank);
+//		// Only process 0 loads the graph, which is distributed automatically
+//		for (unsigned int i = 0; i < edges.size(); ++i) {
+//			add_edge(vertex(edges[i].first, g), vertex(edges[i].second, g), g);
+//		}
+//	}
+//
+//	synchronize(g.process_group());
+//	if (process_id(g.process_group()) == 0) {
+//		cout << "Graph created\n";
+//		stopWatch.stop(stopWatch.inputProcessing);
+//		stopWatch.start(stopWatch.mainSection);
+//	}
+//	property_map<Graph, component_t>::type component_map = get(component_t(), g);
+//
+//	connected_components(g, component_map);
+//
+//	if (process_id(g.process_group()) == 0) {
+//		stopWatch.stop(stopWatch.mainSection);
+//		stopWatch.start(stopWatch.merging);
+//
+//		cout << "Reading result\n";
+//		for (int i = 0; i < numberOfVertices; ++i) {
+//			outVertexToComponent[i] = get(component_map, vertex(i,g));
+//		}
+//	}
+//	synchronize(g.process_group());
+//
+//	if (process_id(g.process_group()) == 0) {
+//		stopWatch.stop(stopWatch.merging);
+//	}
+//	return 0;
+//
+//}
+//#else
 
 int PBoost::run(const int numberOfVertices, const std::vector<std::pair<int,int> > &edges, std::vector<int> &outVertexToComponent, StopWatch &stopWatch) {
-
-	boost::mpi::environment env();
-
-
-	stopWatch.start(stopWatch.inputProcessing);
-
-	Graph g(numberOfVertices);
-	if (process_id(g.process_group()) == 0) {
-		int mpiProcessRank;
-		MPI_Comm_rank (MPI_COMM_WORLD, &mpiProcessRank);
-		// Only process 0 loads the graph, which is distributed automatically
-		for (unsigned int i = 0; i < edges.size(); ++i) {
-			add_edge(vertex(edges[i].first, g), vertex(edges[i].second, g), g);
-		}
-	}
-
-	synchronize(g.process_group());
-	if (process_id(g.process_group()) == 0) {
-		cout << "Graph created\n";
-		stopWatch.stop(stopWatch.inputProcessing);
-		stopWatch.start(stopWatch.mainSection);
-	}
-	property_map<Graph, component_t>::type component_map = get(component_t(), g);
-
-	connected_components(g, component_map);
-
-	if (process_id(g.process_group()) == 0) {
-		stopWatch.stop(stopWatch.mainSection);
-		stopWatch.start(stopWatch.merging);
-
-		cout << "Reading result\n";
-		for (int i = 0; i < numberOfVertices; ++i) {
-			outVertexToComponent[i] = get(component_map, vertex(i,g));
-		}
-	}
-	synchronize(g.process_group());
-
-	if (process_id(g.process_group()) == 0) {
-		stopWatch.stop(stopWatch.merging);
-	}
-	return 0;
-
-}
-#else
-int PBoost::run(const int numberOfVertices, const std::vector<std::pair<int,int> > &edges, std::vector<int> &outVertexToComponent, StopWatch &stopWatch) {
 	return 0;
 }
-#endif
+//#endif
